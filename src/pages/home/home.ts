6e39da88e6659675c82bdaf14de4dcf482e2016e
webosmotic-developer <dev.webosmotic@gmail.com>
2017-11-03 12:40:12 +0530

    Home page changes.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,54 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+
+declare var cordova: any;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public iSlotID: string;
+  public iCabinetID: string;
+  public cabinetArr: any;
+  public slotArr: any;
 
+  constructor() {
+    this.cabinetArr = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
+    this.slotArr = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12',
+      '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'];
+    this.iCabinetID = '00';
+    this.iSlotID = '01';
   }
 
+  /**
+   * Open Locker Slot
+   * */
+  fnOpenSlot() {
+    cordova.plugins.snbc.openSlot(
+      this.iSlotID, this.iCabinetID,
+      (status) => {
+        alert('SUCCESS : <' + this.iCabinetID + ',' + this.iSlotID + '>' + status);
+      },
+      (err) => {
+        alert('ERROR : <' + this.iCabinetID + ',' + this.iSlotID + '>' + err);
+      }
+    );
+  }
+
+  /**
+   * Check slot status
+   * */
+  fnGetSlotStatus() {
+    cordova.plugins.snbc.getLockerStatus(
+      this.iSlotID, this.iCabinetID,
+      (status) => {
+        alert('SUCCESS : <' + this.iCabinetID + ',' + this.iSlotID + '>' + status);
+      },
+      (err) => {
+        alert('ERROR : <' + this.iCabinetID + ',' + this.iSlotID + '>' + err);
+      }
+    );
+  }
 }
